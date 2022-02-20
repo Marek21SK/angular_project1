b9f79cd7925e16451447d8c8f28e98499d636151
Marek Pástor <marekpastorml@gmail.com>
2022-02-20 20:09:50 +0100

    Spravený Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project1';
+  title = 'Úloha1 (Vytvorenie spolocneho angular projektu)';
+
 }
