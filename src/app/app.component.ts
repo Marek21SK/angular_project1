5cf3f6ff5fdca7443f3cceb74f9e61ec961e3d5b
Marek Pástor <marekpastorml@gmail.com>
2022-02-25 18:51:36 +0100

    Logic fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,45 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Úloha1 (Vytvorenie spolocneho angular projektu)';
-  bin = " ";
   decimal = 0;
   n = 0;
 
+  bits: boolean[] = [false, false, false, false, false, false, false, false];
 
   public transfertoBin(){
-    this.bin = ((this.decimal >>> 0).toString(2));
-    let o = this.bin.length;
-    var pole:number[] = new Array(o);
-    var pom_pole:number[] = new Array(o);
-    let pom =1;
-    for (let n=0;n<this.bin.length;n++){
-      var st = this.bin;
-      var sliced = st.slice(o-1,o);
-      pole[n] = Number(sliced);
-      pom_pole[o-1] = pole[n]
-      o--;
-    }
-    alert(pom_pole);
-  }
-
-  public checkboxes(){
-    let o = this.bin.length;
-    var pole:number[] = new Array(o);
-    let pom =1;
-    for (let n=0;n<this.bin.length;n++){
-      var st = this.bin;
-      var sliced = st.slice(o-1,o);
-      pole[n] = Number(sliced);
-    }
-    alert(pole)
-    for (let n =0;n<this.bin.length;n++){
-      let select = document.getElementById(String(pom));
-      if (pole[o]==1){
-
-      }
-      pom++;
-      o--;
+    let binary = ((this.decimal >>> 0).toString(2)).padStart(8, '0');
+    for (let n=0;n< binary.length;n++){
+      this.bits[7 - n] = n < binary.length && binary[n] == '1';
     }
   }
-
 }
