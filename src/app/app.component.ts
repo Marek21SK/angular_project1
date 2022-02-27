bae950a3148209226c81985347374e2a5d96a6c1
Marek Pástor <marekpastorml@gmail.com>
2022-02-27 13:07:31 +0100

    Pridanie zoznamu OSOBY a KNIHY + Funkcionalita.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 
+enum MENU {OSOBY, KNIHY,VYPOZICKY}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Úloha1 (Vytvorenie spolocneho angular projektu)';
+  title = 'Úloha1 a Úloha2 (Vytvorenie spoločného angular projektu.)';
+
+  osoby: any = [];
+  osoba = {id:" ", meno:" ", kontakt:" "};
+
+  knihy: any = [];
+  kniha = {id:" ", nazov:" ", autor:" ", dostupnost:" "};
+
+  menu = MENU;
+  aktMenu: MENU = MENU.OSOBY;
+
+  nastaMenu(m: MENU){
+    this.aktMenu = m;
+  }
+
+  public pridaj(): void{
+    this.osoby.push({id: this.osoba.id, meno: this.osoba.meno, kontakt: this.osoba.kontakt});
+  }
+
+  public pridaj2(): void{
+    this.knihy.push({id: this.kniha.id, nazov: this.kniha.nazov, autor: this.kniha.autor, dostupnost: this.kniha.dostupnost});
+  }
+
   decimal = 0;
   n = 0;
 
