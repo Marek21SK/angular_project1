e09d88b8777a554c0c2bab05cc286a081edaa0bb
davidHajdu <davidhajdu25@gmail.com>
2022-03-14 14:55:37 +0100

    mazanie kníh fix

--- src/app/kniha-stranka/kniha-stranka.component.ts
+++ src/app/kniha-stranka/kniha-stranka.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import {Kniha} from "../models/kniha.model";
 
 @Component({
-  selector: 'app-osoba-stranka',
-  templateUrl: './osoba-stranka.component.html',
-  styleUrls: ['./osoba-stranka.component.css']
+  selector: 'app-kniha-stranka',
+  templateUrl: './kniha-stranka.component.html',
+  styleUrls: ['./kniha-stranka.component.css']
 })
-export class OsobaStrankaComponent {
+export class KnihaStrankaComponent {
 
-  knihy: Kniha[] = [];
+  knihy: Kniha[] = []
 
-  zmazZoZoznamu(kniha: Kniha): void {
+  zmazZoZoznamu(kniha: Kniha): void{
     const index = this.knihy.findIndex(knihaArray => knihaArray.id === kniha.id);
     if (index !== -1) {
       this.knihy.splice(index, 1);
